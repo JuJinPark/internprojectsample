266ad22f0ad057ca0c36b8fffc2fab4c16e5c16d
JuJinPark <wnwls1216@gmail.com>
2019-08-13 21:46:13 +0900

    빈객체꺼내기

--- backend/src/main/java/com/gabia/internproject/InternprojectApplication.java
+++ backend/src/main/java/com/gabia/internproject/InternprojectApplication.java
@@ -1,16 +1,23 @@
 package com.gabia.internproject;
 
+import com.gabia.internproject.data.repository.UserRepository;
+import com.gabia.internproject.service.DataBaseUserService;
+import com.gabia.internproject.service.HiworksUserService;
 import com.gabia.internproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class InternprojectApplication {
+public class InternprojectApplication implements CommandLineRunner {
 
-
+	@Autowired
+	private ApplicationContext context;
 
 
 	public static void main(String[] args) {
@@ -18,8 +25,24 @@ public class InternprojectApplication {
 //		System.setProperty("spring.devtools.livereload.enabled","true");
 
 		SpringApplication.run(InternprojectApplication.class, args);
+
+
 //crossorgin 설정해주기
 
 	}
+
+	public void run(String... args) throws Exception{
+		String[] beans = context.getBeanDefinitionNames();
+		Arrays.sort(beans);
+		for (String bean : beans) {
+			System.out.println(bean);
+		}
+
+		System.out.println(context.getBean(HiworksUserService.class));
+		System.out.println(context.getBean(DataBaseUserService.class));
+
+	}
+
+
 
 }
